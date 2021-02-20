1ffb5b4265061eb93ff03a2b1a7a462b702c1a0a
Russell <russelldev@protonmail.com>
2021-02-20 15:42:22 +0000

    Add getPokemon()

--- app.js
+++ app.js
@@ -22,3 +22,9 @@ const fetchPokemons = async () => {
         await getPokemon(i);
     };
 };
+
+const getPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url);
+    const data = await res.json();
+};
